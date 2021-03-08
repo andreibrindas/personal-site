cb72c35cb873576056f00b4f99f125813e19c596
andreibrindas <andreibrindas@users.noreply.github.com>
2021-03-08 15:00:24 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  siteMetadata: {
+    title: "andreibrindas",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        url: "blog.andreibrindas.com",
+      },
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
